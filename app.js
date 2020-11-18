b4ccafea8f1f7bfddead6ce66a5edd1d7522ea60
Thomas Vinther <tvinther1@live.dk>
2020-11-18 13:52:13 +0100

    first commit

--- app.js
+++ app.js
@@ -22,5 +22,34 @@ app.use(express.static(path));
 app.use('/', router);
 
 app.listen(port, function () {
-  console.log(`I\'m listening for you on port ${port}!`)
-})
+    console.log(`I\'m listening for you on port ${port}!`)
+});
+
+app.get('/searching', function (req, res) {
+    var mysql = require('mysql');
+
+    var con = mysql.createConnection({
+        host: "mysql78.unoeuro.com",
+        user: "multicrypt_io",
+        password: "D2gnzGrdcamy",
+        database: "multicrypt_io_db"
+    });
+
+    var lot = req.query.lot;
+    console.log(lot);
+
+    var action = req.query.action;
+    console.log(action);
+
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
+        con.query("insert into test (text) values ('"+lot+"-"+action+"');", function (err, result) {
+            if (err) throw err;
+            console.log("ROW INSERTED");
+        });
+    });
+
+    res.send("WHEEE (response text)");
+    //con.end();
+});
